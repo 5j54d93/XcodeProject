5fa9981e354d5b871debb2733cf46aa788879e12
Ricky Chuang <songsongyyohya@gmail.com>
2023-09-02 22:01:14 +0800

    Update Archive.js

--- Scripts/Archive.js
+++ Scripts/Archive.js
@@ -1,5 +1,10 @@
 document.getElementById("SecondNav").classList.toggle("sticky-top");
+document.addEventListener("click", (event) => {
+    hideArchiveFilterYear();
+    hideArchiveFilterMonth();
+});
 function toggleArchiveFilterYear() {
+    hideArchiveFilterMonth();
     if (archiveFilterYearList.style.display === "none") {
         archiveFilterYearList.style.display = "block";
         archiveFilterYearBtn.style.borderBottomLeftRadius = "0";
@@ -8,15 +13,11 @@ function toggleArchiveFilterYear() {
         archiveFilterYearBtnIcon.classList.remove("bi-chevron-down");
         archiveFilterYearBtnIcon.classList.add("bi-chevron-up");
     } else {
-        archiveFilterYearList.style.display = "none";
-        archiveFilterYearBtn.style.borderBottomLeftRadius = "7px";
-        archiveFilterYearBtn.style.borderBottomRightRadius = "7px";
-        archiveFilterYearBtn.style.boxShadow = "0 0 0 rgba(0,0,0,0.2)";
-        archiveFilterYearBtnIcon.classList.remove("bi-chevron-up");
-        archiveFilterYearBtnIcon.classList.add("bi-chevron-down");
+        hideArchiveFilterYear();
     }
 }
 function toggleArchiveFilterMonth() {
+    hideArchiveFilterYear();
     if (archiveFilterMonthList.style.display === "none") {
         archiveFilterMonthList.style.display = "block";
         archiveFilterMonthBtn.style.borderBottomLeftRadius = "0";
@@ -25,11 +26,22 @@ function toggleArchiveFilterMonth() {
         archiveFilterMonthBtnIcon.classList.remove("bi-chevron-down");
         archiveFilterMonthBtnIcon.classList.add("bi-chevron-up");
     } else {
-        archiveFilterMonthList.style.display = "none";
-        archiveFilterMonthBtn.style.borderBottomLeftRadius = "7px";
-        archiveFilterMonthBtn.style.borderBottomRightRadius = "7px";
-        archiveFilterMonthBtn.style.boxShadow = "0 0 0 rgba(0,0,0,0.2)";
-        archiveFilterMonthBtnIcon.classList.remove("bi-chevron-up");
-        archiveFilterMonthBtnIcon.classList.add("bi-chevron-down");
+        hideArchiveFilterMonth();
     }
+}
+function hideArchiveFilterYear() {
+    archiveFilterYearList.style.display = "none";
+    archiveFilterYearBtn.style.borderBottomLeftRadius = "7px";
+    archiveFilterYearBtn.style.borderBottomRightRadius = "7px";
+    archiveFilterYearBtn.style.boxShadow = "0 0 0 rgba(0,0,0,0.2)";
+    archiveFilterYearBtnIcon.classList.remove("bi-chevron-up");
+    archiveFilterYearBtnIcon.classList.add("bi-chevron-down");
+}
+function hideArchiveFilterMonth() {
+    archiveFilterMonthList.style.display = "none";
+    archiveFilterMonthBtn.style.borderBottomLeftRadius = "7px";
+    archiveFilterMonthBtn.style.borderBottomRightRadius = "7px";
+    archiveFilterMonthBtn.style.boxShadow = "0 0 0 rgba(0,0,0,0.2)";
+    archiveFilterMonthBtnIcon.classList.remove("bi-chevron-up");
+    archiveFilterMonthBtnIcon.classList.add("bi-chevron-down");
 }
