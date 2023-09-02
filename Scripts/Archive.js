1d484da47826f07289de228ddcdf1815ae751126
Ricky Chuang <songsongyyohya@gmail.com>
2023-09-02 21:37:33 +0800

    Update Archive.js

--- Scripts/Archive.js
+++ Scripts/Archive.js
@@ -1,3 +1,4 @@
+document.getElementById("SecondNav").classList.toggle("sticky-top");
 function toggleArchiveFilterYear() {
     if (archiveFilterYearList.style.display === "none") {
         archiveFilterYearList.style.display = "block";
@@ -13,5 +14,22 @@ function toggleArchiveFilterYear() {
         archiveFilterYearBtn.style.boxShadow = "0 0 0 rgba(0,0,0,0.2)";
         archiveFilterYearBtnIcon.classList.remove("bi-chevron-up");
         archiveFilterYearBtnIcon.classList.add("bi-chevron-down");
+    }
+}
+function toggleArchiveFilterMonth() {
+    if (archiveFilterMonthList.style.display === "none") {
+        archiveFilterMonthList.style.display = "block";
+        archiveFilterMonthBtn.style.borderBottomLeftRadius = "0";
+        archiveFilterMonthBtn.style.borderBottomRightRadius = "0";
+        archiveFilterMonthBtn.style.boxShadow = "0 0 10px rgba(0,0,0,0.2)";
+        archiveFilterMonthBtnIcon.classList.remove("bi-chevron-down");
+        archiveFilterMonthBtnIcon.classList.add("bi-chevron-up");
+    } else {
+        archiveFilterMonthList.style.display = "none";
+        archiveFilterMonthBtn.style.borderBottomLeftRadius = "7px";
+        archiveFilterMonthBtn.style.borderBottomRightRadius = "7px";
+        archiveFilterMonthBtn.style.boxShadow = "0 0 0 rgba(0,0,0,0.2)";
+        archiveFilterMonthBtnIcon.classList.remove("bi-chevron-up");
+        archiveFilterMonthBtnIcon.classList.add("bi-chevron-down");
     }
 }
