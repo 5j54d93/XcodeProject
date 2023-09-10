fb1f35200e8eae5f4c10f6deeec340f956f31e6a
Ricky Chuang <songsongyyohya@gmail.com>
2023-09-10 22:52:18 +0800

    Update Footer.js

--- Scripts/Footer.js
+++ Scripts/Footer.js
@@ -1,3 +1,6 @@
+const date = new Date();
+footerCopyright.innerText = "Copyright © " + date.getFullYear() + " XcodeProject Inc. 保留一切權利。"
+
 function rotate(id) {
     const obj = document.getElementById(String(id));
     if (obj.style.transform === "rotate(45deg) scale(1.08)") {
