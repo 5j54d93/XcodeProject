75345b107fae06899b4c9f2cd303c2cffe5430e3
Ricky Chuang <5j54d93@gmail.com>
2024-07-07 00:38:43 +0800

    Update SecondNav.js

--- Scripts/SecondNav.js
+++ Scripts/SecondNav.js
@@ -6,7 +6,7 @@ hotTopics.addEventListener('show.bs.collapse', event => {
     secondNav.style.backgroundColor = "rgb(255, 255, 255, 0.9)";
     hotTopicsDimBackground.style.display = "block";
     secondNavSearchBtn.innerHTML = "<i class='bi bi-x-lg' style='font-size: 10px; margin-right: 3.6px; -webkit-text-stroke: 0.5px;'></i>Close";
-    hotTopicsSearchBar.style.opacity = "1";
+    hotTopicsSearchForm.style.opacity = "1";
     hotTopics.style.top = "";
     hotTopics.style.bottom = "";
 })
@@ -20,11 +20,11 @@ hotTopics.addEventListener('shown.bs.collapse', event => {
 })
 // 熱門話題「開始隱藏」
 hotTopics.addEventListener('hide.bs.collapse', event => {
-    hotTopicsSearchBar.style.opacity = "0";
+    hotTopicsSearchForm.style.opacity = "0";
+    secondNav.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
     document.body.style.position = "";
     document.body.style.width = "auto";
     mainNav.style.display = "";
-    secondNav.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
     hotTopicsDimBackground.style.display = "none";
     secondNavSearchBtn.innerHTML = "<i class='bi bi-search' style='font-size: 10px; margin-right: 3.6px;'></i>搜尋<span class='d-none d-md-inline'> XcodeProject</span>";
 })
