3b5771d063de37e48783826c24d3271850d99cd4
Ricky Chuang <5j54d93@gmail.com>
2024-07-07 00:53:43 +0800

    Update SecondNav.js

--- Scripts/SecondNav.js
+++ Scripts/SecondNav.js
@@ -1,6 +1,5 @@
 // 熱門話題「開始出現」
 hotTopics.addEventListener('show.bs.collapse', event => {
-    hotTopics.style.zIndex = "1022";
     document.body.style.position = "fixed";
     document.body.style.width = "100%";
     mainNav.style.display = "none";
@@ -22,7 +21,6 @@ hotTopics.addEventListener('shown.bs.collapse', event => {
 // 熱門話題「開始隱藏」
 hotTopics.addEventListener('hide.bs.collapse', event => {
     hotTopicsSearchForm.style.opacity = "0";
-    hotTopics.style.zIndex = "0";
     secondNav.style.backgroundColor = "rgba(255, 255, 255, 0.7)";
     document.body.style.position = "";
     document.body.style.width = "auto";
