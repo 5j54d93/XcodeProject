8d18f07d64cc1e7db78ce578fc0d3e2b603ef9b0
Ricky Chuang <5j54d93@gmail.com>
2024-07-06 22:30:27 +0800

    Update Label.js

--- Scripts/Label.js
+++ Scripts/Label.js
@@ -1,11 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
     const labelPagePosts = document.querySelectorAll(".labelPagePost");
+    labelPagePosts[0].classList.add('show');
     labelPagePosts.forEach(labelPagePost => {
         var position = labelPagePost.getBoundingClientRect().top / window.innerHeight;
         if (position >= 0 && position <= 1) {
             labelPagePost.classList.add('show');
         }
     });
+    labelPageTopFollowAlert.style.display = sessionStorage.getItem("isCloseFollowAlert") ? "none" : "";
 });
 window.addEventListener("scroll", () => {
     const labelPagePosts = document.querySelectorAll(".labelPagePost");
