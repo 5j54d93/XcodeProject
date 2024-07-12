2ae46fcf341a99f8b3de95a8ebbbbd22ecdaabec
Ricky Chuang <5j54d93@gmail.com>
2024-07-12 22:50:51 +0800

    Update Post.js

--- Scripts/Post.js
+++ Scripts/Post.js
@@ -1,5 +1,27 @@
-window.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     blogPostTopFollowAlert.style.display = sessionStorage.getItem("isCloseFollowAlert") ? "none" : "";
+    var lazyImages = [].slice.call(document.querySelectorAll(".blogPostImg"));
+    if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.srcset = lazyImage.dataset.srcset;
+                    lazyImage.onload = function () {
+                        lazyImage.parentElement.background = "";
+                        lazyImage.parentElement.aspectRatio = "";
+                        lazyImage.parentElement.borderRadius = "";
+                    }
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+        
+        lazyImages.forEach(function(lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    }
 });
 function copyPostUrl(id) {
     navigator.clipboard.writeText(location.protocol + '//' + location.host + location.pathname);
