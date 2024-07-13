a8d7ef56f498bd4b49e1847354fb4cf5cda46589
Ricky Chuang <5j54d93@gmail.com>
2024-07-13 10:22:00 +0800

    Update Home.js

--- Scripts/Home.js
+++ Scripts/Home.js
@@ -1,17 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     homeTopFollowAlert.style.display = sessionStorage.getItem("isCloseFollowAlert") ? "none" : "";
+    
     const homeNewPosts = document.querySelectorAll(".homeNewPost");
+    const homePopularPosts = document.querySelectorAll(".homePopularPost");
+    
     let intersectionObserver = new IntersectionObserver(function(entries, observer) {
         entries.forEach(function(entry) {
             if (entry.intersectionRatio > 0) {
-                let homeNewPost = entry.target;
-                homeNewPost.classList.add('show');
-                intersectionObserver.unobserve(homeNewPost);
+                let target = entry.target;
+                target.classList.add('show');
+                intersectionObserver.unobserve(target);
             }
         });
     });
     
     homeNewPosts.forEach(function(homeNewPost) {
         intersectionObserver.observe(homeNewPost);
+    });
+    homePopularPosts.forEach(function(homePopularPost) {
+        intersectionObserver.observe(homePopularPost);
     });
 });
