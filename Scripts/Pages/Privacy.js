7e7219d84154fc6ac8bc9b5bdac71f7b0f660eb0
Ricky Chuang <5j54d93@gmail.com>
2024-07-20 13:58:34 +0800

    Create Privacy.js

---
+++ Scripts/Pages/Privacy.js
@@ -0,0 +1,36 @@
+Privacy1.addEventListener('show.bs.collapse', event => {
+    Privacy1Ｘ.style.transform = "rotate(45deg) scale(1.08)";
+})
+Privacy1.addEventListener('hide.bs.collapse', event => {
+    Privacy1Ｘ.style.transform = "rotate(0) scale(1)";
+})
+Privacy2.addEventListener('show.bs.collapse', event => {
+    Privacy2Ｘ.style.transform = "rotate(45deg) scale(1.08)";
+})
+Privacy2.addEventListener('hide.bs.collapse', event => {
+    Privacy2Ｘ.style.transform = "rotate(0) scale(1)";
+})
+Privacy3.addEventListener('show.bs.collapse', event => {
+    Privacy3Ｘ.style.transform = "rotate(45deg) scale(1.08)";
+})
+Privacy3.addEventListener('hide.bs.collapse', event => {
+    Privacy3Ｘ.style.transform = "rotate(0) scale(1)";
+})
+Privacy4.addEventListener('show.bs.collapse', event => {
+    Privacy4Ｘ.style.transform = "rotate(45deg) scale(1.08)";
+})
+Privacy4.addEventListener('hide.bs.collapse', event => {
+    Privacy4Ｘ.style.transform = "rotate(0) scale(1)";
+})
+Privacy5.addEventListener('show.bs.collapse', event => {
+    Privacy5Ｘ.style.transform = "rotate(45deg) scale(1.08)";
+})
+Privacy5.addEventListener('hide.bs.collapse', event => {
+    Privacy5Ｘ.style.transform = "rotate(0) scale(1)";
+})
+Privacy6.addEventListener('show.bs.collapse', event => {
+    Privacy6Ｘ.style.transform = "rotate(45deg) scale(1.08)";
+})
+Privacy6.addEventListener('hide.bs.collapse', event => {
+    Privacy6Ｘ.style.transform = "rotate(0) scale(1)";
+})
