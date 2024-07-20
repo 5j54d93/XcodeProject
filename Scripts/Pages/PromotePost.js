00ca03924a4ab3f533fd8d7379be533c1d962e02
Ricky Chuang <5j54d93@gmail.com>
2024-07-20 19:34:40 +0800

    Create PromotePost.js

---
+++ Scripts/Pages/PromotePost.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var lazyImages = document.querySelectorAll(".img-fluid");
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.intersectionRatio > 0) {
+                let lazyImage = entry.target;
+                lazyImage.onload = function () {
+                    lazyImage.dataset.src = "";
+                    lazyImage.parentElement.classList.remove("laodingImgContainer");
+                }
+                lazyImage.src = lazyImage.dataset.src;
+                lazyImageObserver.unobserve(lazyImage);
+            }
+        });
+    });
+    
+    lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+    });
+});
+function reloadImage(reloadButton) {
+    reloadButton.disabled = true;
+    const targetImage = reloadButton.parentElement.previousElementSibling.firstElementChild;
+    var imageSrc = "";
+    if (targetImage.src.length !== 0) {
+      	imageSrc = targetImage.src;
+    } else if (targetImage.dataset.src.length !== 0) {
+      	imageSrc = targetImage.dataset.src;
+    }
+    targetImage.removeAttribute('src');
+    targetImage.classList.add('laodingImgContainer');
+    targetImage.onload = () => {
+        targetImage.dataset.src = "";
+        targetImage.classList.remove('laodingImgContainer');
+    }
+    setTimeout(() => {
+  		targetImage.src = imageSrc;
+        reloadButton.disabled = false;
+	}, 800);
+}
+function copyPostUrl(id) {
+    navigator.clipboard.writeText(location.protocol + '//' + location.host + location.pathname);
+    const obj = document.getElementById(String(id));
+    obj.style.opacity = "1";
+    setTimeout( () => {
+        obj.style.opacity = "0";
+    }, 1000);
+}
