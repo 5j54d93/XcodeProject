922ce678b5783c0d4294c2dc07dad8f416812ae6
Ricky Chuang <5j54d93@gmail.com>
2024-07-21 00:11:25 +0800

    Update Common.js

--- Scripts/Common.js
+++ Scripts/Common.js
@@ -1,3 +1,8 @@
+var uri = window.location.toString();
+if (uri.indexOf("?m=1", "?m=1") > 0) {
+    var cleanUri = uri.substring(0, uri.indexOf("?m=1"));
+    window.history.replaceState({}, document.title, cleanUri);
+}
 window.addEventListener('resize', () => {
     if (window.innerWidth > 768 && hamburgerBtn.ariaExpanded === "true") { // 全螢幕導覽列展開中
         var 全螢幕導覽列收合元件 = new bootstrap.Collapse(fullScreenNav);
