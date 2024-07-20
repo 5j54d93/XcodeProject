399b10ca29087c7d77b3383a85a5040a3f995b7a
Ricky Chuang <5j54d93@gmail.com>
2024-07-20 13:34:04 +0800

    Update Footer.js

--- Scripts/Footer.js
+++ Scripts/Footer.js
@@ -1,11 +1,39 @@
 const footerDate = new Date();
 footerCopyright.innerText = "Copyright © " + footerDate.getFullYear() + " XcodeProject Inc. 保留一切權利。"
 
-function rotate(id) {
-    const obj = document.getElementById(String(id));
-    if (obj.style.transform === "rotate(45deg) scale(1.08)") {
-        obj.style.transform = "rotate(0) scale(1)";
-    } else {
-        obj.style.transform = "rotate(45deg) scale(1.08)";
-    }
-}
+平台.addEventListener('show.bs.collapse', event => {
+    平台x.style.transform = "rotate(45deg) scale(1.08)";
+})
+平台.addEventListener('hide.bs.collapse', event => {
+    平台x.style.transform = "rotate(0) scale(1)";
+})
+工具.addEventListener('show.bs.collapse', event => {
+    工具x.style.transform = "rotate(45deg) scale(1.08)";
+})
+工具.addEventListener('hide.bs.collapse', event => {
+    工具x.style.transform = "rotate(0) scale(1)";
+})
+社群平台.addEventListener('show.bs.collapse', event => {
+    社群平台x.style.transform = "rotate(45deg) scale(1.08)";
+})
+社群平台.addEventListener('hide.bs.collapse', event => {
+    社群平台x.style.transform = "rotate(0) scale(1)";
+})
+主題＆技術.addEventListener('show.bs.collapse', event => {
+    主題＆技術x.style.transform = "rotate(45deg) scale(1.08)";
+})
+主題＆技術.addEventListener('hide.bs.collapse', event => {
+    主題＆技術x.style.transform = "rotate(0) scale(1)";
+})
+理念價值.addEventListener('show.bs.collapse', event => {
+    理念價值x.style.transform = "rotate(45deg) scale(1.08)";
+})
+理念價值.addEventListener('hide.bs.collapse', event => {
+    理念價值x.style.transform = "rotate(0) scale(1)";
+})
+關於.addEventListener('show.bs.collapse', event => {
+    關於x.style.transform = "rotate(45deg) scale(1.08)";
+})
+關於.addEventListener('hide.bs.collapse', event => {
+    關於x.style.transform = "rotate(0) scale(1)";
+})
