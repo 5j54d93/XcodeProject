be03fb67f9825f0ee4e1e36bc2e076c7f1ed1995
Ricky Chuang <5j54d93@gmail.com>
2024-07-21 13:51:43 +0800

    Update Invitation.js

--- Scripts/Pages/Invitation.js
+++ Scripts/Pages/Invitation.js
@@ -25,3 +25,24 @@ function onCopy(text) {
         hadCopyTip.style.opacity = "0";
     }, 1000);
 }
+function reloadImage(reloadButton) {
+    reloadButton.disabled = true;
+    const targetImageContainer = reloadButton.parentElement.previousElementSibling;
+    const targetImage = reloadButton.parentElement.previousElementSibling.firstElementChild;
+    var imageSrc = "";
+    if (targetImage.src.length !== 0) {
+      	imageSrc = targetImage.src;
+    } else if (targetImage.dataset.src.length !== 0) {
+      	imageSrc = targetImage.dataset.src;
+    }
+    targetImageContainer.classList.add('loadingImgContainer');
+    targetImage.removeAttribute('src');
+    targetImage.onload = () => {
+        targetImage.dataset.src = "";
+        targetImageContainer.classList.remove('loadingImgContainer');
+    }
+    setTimeout(() => {
+  		targetImage.src = imageSrc;
+        reloadButton.disabled = false;
+	}, 800);
+}
