c7ec32fe003cbbe1014c819d1058bb0c41e420ab
Ricky Chuang <5j54d93@gmail.com>
2024-07-22 02:49:12 +0800

    Update Invitation.js

--- Scripts/Pages/Invitation.js
+++ Scripts/Pages/Invitation.js
@@ -28,8 +28,10 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }, { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] });
     floatCapsuleObserver.observe(floatCapsuleLink);
-    floatCapsuleText.addEventListener('shown.bs.collapse', event => {
+    floatCapsuleText.addEventListener('show.bs.collapse', event => {
         floatCapsuleText.style.paddingLeft = "14px";
+    });
+    floatCapsuleText.addEventListener('shown.bs.collapse', event => {
         floatCapsuleText.classList.remove("text-nowrap");
     });
     floatCapsuleText.addEventListener('hide.bs.collapse', event => {
