58957d6a427026a927ae44ee76c4f34fa67cdc6f
Ricky Chuang <5j54d93@gmail.com>
2026-06-05 22:44:57 +0800

    Refactor collapse event listeners into a binding function

--- Scripts/Pages/Privacy.js
+++ Scripts/Pages/Privacy.js
@@ -1,36 +1,8 @@
-Privacy1.addEventListener('show.bs.collapse', event => {
-    Privacy1Ｘ.style.transform = "rotate(45deg) scale(1.08)";
-})
-Privacy1.addEventListener('hide.bs.collapse', event => {
-    Privacy1Ｘ.style.transform = "rotate(0) scale(1)";
-})
-Privacy2.addEventListener('show.bs.collapse', event => {
-    Privacy2Ｘ.style.transform = "rotate(45deg) scale(1.08)";
-})
-Privacy2.addEventListener('hide.bs.collapse', event => {
-    Privacy2Ｘ.style.transform = "rotate(0) scale(1)";
-})
-Privacy3.addEventListener('show.bs.collapse', event => {
-    Privacy3Ｘ.style.transform = "rotate(45deg) scale(1.08)";
-})
-Privacy3.addEventListener('hide.bs.collapse', event => {
-    Privacy3Ｘ.style.transform = "rotate(0) scale(1)";
-})
-Privacy4.addEventListener('show.bs.collapse', event => {
-    Privacy4Ｘ.style.transform = "rotate(45deg) scale(1.08)";
-})
-Privacy4.addEventListener('hide.bs.collapse', event => {
-    Privacy4Ｘ.style.transform = "rotate(0) scale(1)";
-})
-Privacy5.addEventListener('show.bs.collapse', event => {
-    Privacy5Ｘ.style.transform = "rotate(45deg) scale(1.08)";
-})
-Privacy5.addEventListener('hide.bs.collapse', event => {
-    Privacy5Ｘ.style.transform = "rotate(0) scale(1)";
-})
-Privacy6.addEventListener('show.bs.collapse', event => {
-    Privacy6Ｘ.style.transform = "rotate(45deg) scale(1.08)";
-})
-Privacy6.addEventListener('hide.bs.collapse', event => {
-    Privacy6Ｘ.style.transform = "rotate(0) scale(1)";
-})
+window.xcp.bindCollapseIcons([
+    ["Privacy1", "Privacy1Ｘ"],
+    ["Privacy2", "Privacy2Ｘ"],
+    ["Privacy3", "Privacy3Ｘ"],
+    ["Privacy4", "Privacy4Ｘ"],
+    ["Privacy5", "Privacy5Ｘ"],
+    ["Privacy6", "Privacy6Ｘ"],
+]);
