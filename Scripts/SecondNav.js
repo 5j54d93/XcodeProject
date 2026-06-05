c34317f4e753817dff8ad0a0e11eb19d98e43aaf
Ricky Chuang <5j54d93@gmail.com>
2026-06-05 22:20:58 +0800

    Add scroll behavior handling for hot topics collapse

--- Scripts/SecondNav.js
+++ Scripts/SecondNav.js
@@ -1,6 +1,10 @@
+let hotTopicsScrollY = 0;
+
 // 熱門話題「開始出現」
 hotTopics.addEventListener('show.bs.collapse', event => {
+    hotTopicsScrollY = window.scrollY;
     document.body.style.position = "fixed";
+    document.body.style.top = `-${hotTopicsScrollY}px`;
     document.body.style.width = "100%";
     mainNav.style.display = "none";
     secondNav.style.backgroundColor = "#ffffffe6";
@@ -22,9 +26,18 @@ hotTopics.addEventListener('shown.bs.collapse', event => {
 hotTopics.addEventListener('hide.bs.collapse', event => {
     hotTopicsSearchForm.style.opacity = "0";
     secondNav.style.backgroundColor = "#ffffffb3";
-    document.body.style.position = "";
-    document.body.style.width = "auto";
     mainNav.style.display = "";
     hotTopicsDimBackground.style.display = "none";
     secondNavSearchBtn.innerHTML = "<i class='bi bi-search' style='font-size: 10px; margin-right: 3.6px;'></i>搜尋<span class='d-none d-md-inline'> XcodeProject</span>";
+})
+
+// 熱門話題「隱藏完」
+hotTopics.addEventListener('hidden.bs.collapse', event => {
+    const originalScrollBehavior = document.documentElement.style.scrollBehavior;
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.width = "";
+    document.documentElement.style.scrollBehavior = "auto";
+    window.scrollTo(0, hotTopicsScrollY);
+    document.documentElement.style.scrollBehavior = originalScrollBehavior;
 })
